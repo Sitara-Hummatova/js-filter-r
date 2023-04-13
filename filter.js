ddf371c26088fd7e382d93b724daecc5f44822c2
Sitara-Hummatova <sarahummet6471@gmail.com>
2023-04-13 15:17:28 +0400

    hamisi filtrelendi

---
+++ filter.js
@@ -0,0 +1,76 @@
+const data = [
+    { id: 1, name: "Ramzi", surname: "Mammadli", age: 27, MusteriNO: 15, located: "Baku", sifarisSayi: 1020},
+    { id: 2, name: "Heydar", surname: "Mammadli", age: 3, MusteriNO: 12, located: "Sumqayit", sifarisSayi: 151 },
+    { id: 3, name: "John", surname: "Wick", age: 21, MusteriNO: 12, located: "Gence", sifarisSayi: 1510 },
+    { id: 4, name: "Ilqar", surname: "Abdurahmanov", age: 31, MusteriNO: 13, located: "Gence", sifarisSayi: 1320 },
+    { id: 5, name: "Tural", surname: "Rustamov", age: 29, MusteriNO: 13, located: "Baku", sifarisSayi: 1015 },
+    { id: 6, name: "Sitare", surname: "Hummetova", age: 27, MusteriNO: 13, located: "Baku", sifarisSayi: 952 },
+    { id: 7, name: "Terlale", surname: "Ramazanova", age: 21, MusteriNO: 18, located: "Gence", sifarisSayi: 698 },
+    { id: 8, name: "Rauf", surname: "Qarayev", age: 27, MusteriNO: 18, located: "Baku", sifarisSayi: 1205 },
+    { id: 9, name: "Recep", surname: "Tayyip", age: 18, MusteriNO: 20, located: "Baku", sifarisSayi: 125 },
+    { id: 10, name: "Rauf", surname: "Qarayev", age: 27, MusteriNO: 18, located: "Sumqayit", sifarisSayi: 60 },
+    { id: 11, name: "Emine", surname: "Erdogan", age: 18, MusteriNO: 20, located: "Baku", sifarisSayi: 320 },
+    { id: 12, name: "John", surname: "Doe", age: 18, MusteriNO: 18, located: "Sumqayit", sifarisSayi: 195 },
+  ];
+
+
+   //TASK1
+        // Yaşı 21, 27, 18 olanları filterləyin
+        // Müştəri nömrələri 12, 13, 18, 20 olanları filterləyin
+        // Sumqayit, Baki ve Gencede qalanlari filterleyin
+        // SifarisSayi 1000-den yuxari olanlari filterleyin
+        // Sifaris Sayi 1000-den asagi olanlari filterlerin
+
+        // yuxari asagi olanlar boyuk kicik isaresi ile yazilacaq
+
+
+            // Yaşı 21, 27, 18 olanları filterləyin
+        //const Sitare = data.filter((item) => item.age === 21);
+      //console.log(Sitare);
+
+      //const sitare = data.filter((item) => item.age === 27);
+      //console.log(sitare);
+
+      //const sitaree = data.filter((item) => item.age ===18);
+      //console.log(sitaree);
+
+             // Müştəri nömrələri 12, 13, 18, 20 olanları filterləyin
+           
+      //const sara = data.filter((item) => item.MusteriNO === 12);
+      //console.log(sara);
+
+      //const sara = data.filter((item) => item.MusteriNO === 13);
+      //console.log(sara);
+
+      //const sara = data.filter((item) => item.MusteriNO === 18);
+      //console.log(sara);
+
+      //const sara = data.filter((item) => item.MusteriNO === 20);
+      //console.log(sara);
+
+
+
+                   // Sumqayit, Baki ve Gencede qalanlari filterleyin
+
+      //const seherler = data.filter((item) => item.located === "Baku");
+      //console.log(seherler);
+
+      //const seherler = data.filter((item) => item.located === "Gence");
+      //console.log(seherler);
+
+      //const seherler = data.filter((item) => item.located === "Sumqayit");
+      //console.log(seherler);
+
+
+
+           // SifarisSayi 1000-den yuxari olanlari filterleyin
+    
+      //const Sitare = data.filter((item) => item.sifarisSayi > 1000);
+      //console.log(Sitare);
+
+
+      
+            // Sifaris Sayi 1000-den asagi olanlari filterlerin
+
+      //const Sitare = data.filter((item) => item.sifarisSayi < 1000);
+      //console.log(Sitare);
